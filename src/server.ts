f5ae41c6fe0eefea34c3e8233b8bbf157993060a
João Coaglio <joaogabrielmartins1999@gmail.com>
2021-08-24 22:21:53 -0300

    primeira migration

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,11 @@
 import express from "express";
+import "reflect-metadata";
+
+//quando o nome do arquivo da pasta se chama index, não é preciso dizer no import
+import "./database";
 
 const app = express();
 
-app.get("/test", (request, response) => {
-    //Request => entrando
-    //Response => saindo
-   return response.send("Salve");
-});
 
-app.post("/test-post", (request, response) => {
-    return response.send("salve Post");
-})
-
+// 3000 é a porta q irá rodar o projeto
 app.listen(3000, () => console.log("Server is running"));
